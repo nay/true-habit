73e5c1f832de066b1e1eaf80903a90e172d14b66
nay3 <y.ohba@everyleaf.com>
2016-02-23 21:31:21 +0900

    Add twitter authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+  get '/auth/:provider/callback', :to => 'sessions#callback'
+  post '/auth/:provider/callback', :to => 'sessions#callback'
+  get '/logout' => 'sessions#destroy', :as => :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
+
 end
