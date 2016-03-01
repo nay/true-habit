cc93651b6a3cae06766c5c60c2764a5c08ade789
nay3 <y.ohba@everyleaf.com>
2016-03-01 21:11:18 +0900

    Fix session db mismatch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,13 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   private
+
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+      session.clear unless @current_user # DB上に存在しなかったら自動ログアウト
+    end
+    @current_user
   end
 
 end
